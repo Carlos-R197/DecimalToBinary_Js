0260dc5bec4eb1e08cc99d3bc92cb105310debed
Carlos Roque <carlosroque198@gmail.com>
2021-06-27 19:38:51 -0400

    third test passed

--- js/convert.test.js
+++ js/convert.test.js
@@ -4,5 +4,6 @@ describe('Test to convert base 10 numbers to base 2', () => {
     it('should convert base 10 numbers into base 2', () => {
         expect(convert(1)).toBe('1')
         expect(convert(5)).toBe('101')
+        expect(convert(17)).toBe('10001')
     })
 })
